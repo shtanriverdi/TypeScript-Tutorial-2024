2e4f648b73ea393aef4ca1ecfbb2ea833d4f6fc7
shtanriverdi <shtanriverdi@yahoo.com>
2024-08-05 14:25:10 +0300

    Add tuples

--- TypeScript Tutorial 2024/dist/index.js
+++ TypeScript Tutorial 2024/dist/index.js
@@ -1,4 +1,18 @@
 "use strict";
-let a = 5;
-console.log("Genesis");
-a = 1453;
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let sales2 = 123456;
+let course2 = "TypeScript";
+let is_published2 = true;
+let level;
+level = 1;
+level = "a";
+let numbers = [1, 2, 3];
+let numbers2 = [1, 2, 3];
+let anyArr = [];
+anyArr[0] = 2;
+let user = [1, 'Melo'];
+let user2 = [2, 'Seno'];
+user.push(3);
+//# sourceMappingURL=index.js.map
--- TypeScript Tutorial 2024/src/index.ts
+++ TypeScript Tutorial 2024/src/index.ts
@@ -1,3 +1,35 @@
-let a: number = 5;
-console.log("Genesis");
-a = 1453;
+let sales: number = 123_456_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+
+// No need to write annotations
+let sales2 = 123_456;
+let course2 = "TypeScript";
+let is_published2 = true;
+
+// Any
+let level;
+level = 1;
+level = "a";
+
+// Error
+// function render(document) {
+//     console.log(document);
+// }
+
+let numbers: number[] = [1, 2, 3];
+let numbers2 = [1, 2, 3];
+
+let anyArr: number[] = [];
+anyArr[0] = 2
+
+// numbers.forEach(n => n.)
+
+// Tuples, Pair or values
+let user: [number, string] = [1, 'Melo'];
+let user2 = [2, 'Seno'];
+// This is a GAP in TS unfortunately
+// Dont use this!
+user.push(3)
+
+// Enums
